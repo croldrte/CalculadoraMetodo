a202170da429a03c876ad7c0e27274ac885e60b7
Carol Duarte <carolduarte2000@gmail.com>
2025-02-06 11:15:56 -0300

    [feat] Valida entrada da operação

--- Program.cs
+++ Program.cs
@@ -4,53 +4,67 @@ class Program
 {
     static void Main()
     {
-        bool numeroValido;
-        double a, b;
+        bool entradaValida;
+        double a, b, resultado = 0;
+        string operacao;
 
         Console.Clear();
         Console.WriteLine("::: CALCULADORA :::");
+
         Console.Write("Digite o primeiro número: ");
         do
         {
-            numeroValido = double.TryParse(Console.ReadLine(), out a);
-            if (!numeroValido)
+            entradaValida = double.TryParse(Console.ReadLine(), out a);
+            if (!entradaValida)
                 Console.Write("Número inválido! Tente novamente: ");
-        } while (!numeroValido);
+        } while (!entradaValida);
 
         Console.Write("Digite o segundo número: ");
         do
         {
-            numeroValido = double.TryParse(Console.ReadLine(), out b);
-            if (!numeroValido)
+            entradaValida = double.TryParse(Console.ReadLine(), out b);
+            if (!entradaValida)
                 Console.Write("Número inválido! Tente novamente: ");
-        } while (!numeroValido);
+        } while (!entradaValida);
 
         Console.Write("Escolha uma operação (+, -, *, /): ");
-        string operacao = Console.ReadLine();
-        double resultado = 0;
-        switch (operacao)
+        do
         {
-            case "+":
-                resultado = Operacoes.Adicionar(a, b);
-                break;
-            case "-":
-                resultado = Operacoes.Subtrair(a, b);
-                break;
-            case "*":
-                resultado = Operacoes.Multiplicar(a, b);
-                break;
-            case "/":
-                if (b != 0)
-                {
-                    resultado = Operacoes.Dividir(a, b);
+            operacao = Console.ReadLine();
+            switch (operacao)
+            {
+                case "+":
+                    entradaValida = true;
+                    resultado = Operacoes.Adicionar(a, b);
                     break;
-                }
-                else
-                {
-                    Console.WriteLine("Não existe divisão por zero.");
-                    return;
-                }
-        }
-        Console.WriteLine($"O resultado é: {resultado}.");
+                case "-":
+                    entradaValida = true;
+                    resultado = Operacoes.Subtrair(a, b);
+                    break;
+                case "*":
+                    entradaValida = true;
+                    resultado = Operacoes.Multiplicar(a, b);
+                    break;
+                case "/":
+                    if (b != 0)
+                    {
+                        entradaValida = true;
+                        resultado = Operacoes.Dividir(a, b);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não existe divisão por zero.");
+                        return;
+                    }
+                default:
+                    entradaValida = false;
+                    Console.Write("Operação inválida! Tente novamente: ");
+                    break;
+            }
+        } while (!entradaValida);
+        Console.Clear();
+        Console.WriteLine("::: CALCULADORA :::");
+        Console.WriteLine($"{a} {operacao} {b} = {resultado}");
     }
 }
